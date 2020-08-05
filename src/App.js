f5d8b636f963f33980c467a461c8efa6bfafe2a1
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-05 23:05:50 +0500

    Consoling input value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import UserForm from "./components/UserForm";
+
+
+
+
 function App() {
+
+  const getUser=e=>{
+    e.preventDefault();
+    
+    const user = e.target.elements.username.value;
+    console.log(user);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="App-title">HTTP Calls in React</h1>
       </header>
+
+      <UserForm getUser={getUser}/>
     </div>
   );
 }
---
+++ src/components/UserForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+
+const UserForm = (props) => {
+    return (
+        <form onSubmit={props.getUser}>
+            <input 
+            style={{margin:"20px auto",display:"block"}}
+            type="text" 
+            name="username"
+            />
+            <button>SUbmit</button>
+            
+        </form>
+    );
+}
+
+
+
+export default UserForm
